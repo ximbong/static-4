669ff8d6847576ee59794b4a039e08d746065625
Hai Truong <ximbong91023@users.noreply.github.com>
2018-05-23 11:02:26 +0300

    Modify some text patterns, and add checkbox validity check

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const validateText = (id, pattern) => {
     document.getElementById(`${id}`).addEventListener('blur', function(e) {
       const element = e.target;
-      console.log(element.value)
       if (pattern.test(element.value)) {
         element.setCustomValidity('');
         element.classList.add('valid');
       } else {
-        element.setCustomValidity('123 alo');
+        element.setCustomValidity('Invalid');
         element.classList.add('invalid');
       }
     })
@@ -18,9 +17,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
   validateText('email', /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
   validateText('phone', /^(\+?\d{0,3}\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/);
   validateText('address', /\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\./);
-  validateText('country', /[a-zA-Z\s]+/);
-  validateText('state', /[a-zA-Z\s]+/);
-  validateText('city', /[a-zA-Z\s]+/);
+  validateText('country', /^([A-Z][a-z]+)(\s[A-Z][a-z]*)*$/);
+  validateText('state', /^([A-Z][a-z]+)(\s[A-Z][a-z]*)*$/);
+  validateText('city', /^([A-Z][a-z]+)(\s[A-Z][a-z]*)*$/);
   validateText('zipcode', /\d{4,8}/);
 
   document.getElementById(`email2`).addEventListener('blur', function(e) {
@@ -29,8 +28,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
       e.target.setCustomValidity('');
       e.target.classList.add('valid');
     } else {
-      e.target.setCustomValidity('123 alo');
+      e.target.setCustomValidity('Invalid');
       e.target.classList.add('invalid');
     }
   })
+
+  const checkBox = document.querySelectorAll(".checkboxDiv");
+  for (let element of checkBox) {
+    element.addEventListener("click", function(event) {
+      const count = Array.from(element.children).reduce(function(acc, e) {
+        if (e.children[0].checked === true)
+          acc++;
+        console.log(acc)
+        return acc;
+      }, 0);
+      (count === 0)
+        ? element.querySelector('input').setCustomValidity("Please check one option")
+        : element.querySelector('input').setCustomValidity("")
+    });
+  }
 });
